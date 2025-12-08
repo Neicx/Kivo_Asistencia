6d41991d78960537fb028aa94223e21f9d99b1f7
alexceindev <alexceinrp@gmail.com>
2025-12-08 19:25:38 -0300

    q

--- App_Movil/App.js
+++ App_Movil/App.js
@@ -30,7 +30,7 @@ export default function App() {
 
     setLoading(true);
     try {
-      const response = await fetch('http://192.168.56.1:8000/api/token/', {
+      const response = await fetch('http://192.168.1.50:8000/api/token/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ rut: Rut, password: contraseña })
